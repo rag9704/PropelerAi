f971596c87e581b0afc95f25b59f4e706f372ce1
Anurag <anurag@ph.iitr.ac.in>
2023-05-12 22:20:41 +0530

    Update Land_Price_endpoints.py

--- Land_Price_endpoints.py
+++ Land_Price_endpoints.py
@@ -81,4 +81,5 @@ async def get_land_feature(data: Land_Prediction):
 
 if __name__ == '__main__':
         
-    uvicorn.run(app, host='0.0.0.0', port=4000)
+    uvicorn.run(app, host='0.0.0.0', port=8080)
+    
